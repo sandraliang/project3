8221c1fd30fa355a8bfe8a1e2bb88128ff5540c9
sandraliang <sliang01@risd.edu>
2018-01-21 16:16:14 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+$(".production").mouseenter(
+  function (){
+  $(this).attr("src","img/enterhover.gif");
+
+});
+$(".production").mouseleave(
+  function (){
+  $(this).attr("src","img/enter.jpg");
+
+});
+
+
+});
